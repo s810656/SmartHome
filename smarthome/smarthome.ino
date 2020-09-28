ec454dd9312741e42fceda889970444e9b082a5e
s810656 <t99027@gmail.com>
2020-09-28 15:56:15 +0800

    增加蜂鳴器發聲功能

--- smarthome/smarthome.ino
+++ smarthome/smarthome.ino
@@ -7,8 +7,10 @@ void loop()
 {
   digitalWrite(12, HIGH);
   digitalWrite(11, LOW);
+  tone(7, 1000, 500);
   delay(500);
   digitalWrite(12, LOW);
   digitalWrite(11, HIGH);
+  tone(7, 800, 500);
   delay(500);
 }
